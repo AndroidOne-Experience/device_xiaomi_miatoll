14ca16d707077081db7781105a7362215c69a7ef
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-05-16 01:36:31 +0530

    miatoll: Add AndroidOne flags
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- aosp_miatoll.mk
+++ aosp_miatoll.mk
@@ -30,3 +30,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="miatoll_global-user 12 SKQ1.211019.001 V14.0.3.0.SJZMIXM release-keys" \
     BuildFingerprint=Redmi/miatoll_global/miatoll:12/RKQ1.211019.001/V14.0.3.0.SJZMIXM:user/release-keys
+
+# AndroidOne flags
+GMS_VOICE_MODEL_INCLUDED := true
+# AOSP_BUILD_TYPE := PRODUCTION-BUILD # Disabled due to broken OTA feature
