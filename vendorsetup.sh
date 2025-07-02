b642f6ce1c52a00a1f5bf44742ded64de56459e7
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-07-02 05:42:45 +0000

    miatoll: update hardware/xiaomi repo
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,7 +21,7 @@ fi
 git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_sony_timekeep.git hardware/sony/timekeep --depth=1
 
 # hardware/xiaomi
-git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi --depth=1
+git clone -b 15 https://github.com/AndroidOne-Experience/hardware_xiaomi.git hardware/xiaomi --depth=1
 
 # Vendor tree
 git clone -b 15 https://github.com/AndroidOne-Experience/vendor_xiaomi_miatoll.git vendor/xiaomi/miatoll --depth=1
