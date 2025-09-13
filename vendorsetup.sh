0a6a4baf502d7fd3e3f4aa12851d45c971eb6557
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-09-13 16:59:16 +0000

    miatoll: update vendorsetup.sh
    Change-Id: I2252ccfe138c4179524fcb26707021c025f9ceaf
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,10 +21,10 @@ fi
 git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_sony_timekeep.git hardware/sony/timekeep --depth=1
 
 # hardware/xiaomi
-git clone -b 15 https://github.com/AndroidOne-Experience/hardware_xiaomi.git hardware/xiaomi --depth=1
+git clone -b lineage-22.2 https://github.com/LineageOS-miatoll/hardware_xiaomi.git hardware/xiaomi --depth=1
 
 # Vendor tree
-git clone -b 15 https://github.com/AndroidOne-Experience/vendor_xiaomi_miatoll.git vendor/xiaomi/miatoll --depth=1
+git clone -b 15-old https://github.com/AndroidOne-Experience/vendor_xiaomi_miatoll.git vendor/xiaomi/miatoll --depth=1
 
 # Kernel tree
 git clone -b 15 https://github.com/AndroidOne-Experience/kernel_xiaomi_sm6250.git kernel/xiaomi/sm6250 --depth=1
