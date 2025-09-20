1c6705a35708b1cd3cf1fd984ca0dc1352660a00
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-09-20 09:25:24 +0000

    miatoll: Override default animation & duration scale
    Change-Id: Id0777272fc0696ef19aaa5f18f677e97e3839581
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -227,6 +227,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
