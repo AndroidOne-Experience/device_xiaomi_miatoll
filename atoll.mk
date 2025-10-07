bbab3669281de7a2c5e957f4837d41840cf349d5
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-10-07 07:18:59 +0000

    Revert "miatoll: Override default animation & duration scale"
    This reverts commit f5df8d97c07fb4c7ba383c85b796275242dfb74d.

--- atoll.mk
+++ atoll.mk
@@ -251,7 +251,6 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
