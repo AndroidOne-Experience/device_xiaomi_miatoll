cfdd9f0fb264ca046fcc63671e661f9368b1c182
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2026-02-20 11:36:51 +0000

    Revert "miatoll: Setup DeviceAsWebcam"
    This reverts commit d14aef4e4c018ae1b426a1d29291cc3437f24d30.

--- atoll.mk
+++ atoll.mk
@@ -271,7 +271,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
-    SM6250DeviceAsWebcam \
     SM6250Frameworks \
     SM6250SystemUI \
     TelephonyResCommon \
