13d9d3bb8e569cc2f6c1ac72291a3e6876791448
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2026-04-07 14:27:32 +0000

    miatoll: soong_config_set --> soong_config_set_bool
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -62,7 +62,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
 
-$(call soong_config_set,camera,override_format_from_reserved,true)
+$(call soong_config_set_bool,camera,override_format_from_reserved,true)
 
 # Camera (Shim MiuiCamera libs)
 PRODUCT_PACKAGES += libgui_shim_miuicamera
