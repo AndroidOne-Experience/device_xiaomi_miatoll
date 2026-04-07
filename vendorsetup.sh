4c4f3af46af446ee6e58e15b6db8e1f2579d336a
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2026-04-07 13:38:21 +0000

    lineage --> aosp
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# hardware/sony/timekeep
+git clone -b lineage-23.2 https://github.com/LineageOS/android_hardware_sony_timekeep.git hardware/sony/timekeep --depth=1
+
+# hardware/xiaomi
+git clone -b 16-QPR2 https://github.com/AndroidOne-Experience/hardware_xiaomi.git hardware/xiaomi --depth=1
+
+# Vendor tree
+git clone -b 16-QPR2 https://github.com/AndroidOne-Experience/vendor_xiaomi_miatoll.git vendor/xiaomi/miatoll --depth=1
+
+# Kernel tree
+git clone -b 16-QPR2 https://github.com/AndroidOne-Experience/kernel_xiaomi_sm6250.git kernel/xiaomi/sm6250 --depth=1
